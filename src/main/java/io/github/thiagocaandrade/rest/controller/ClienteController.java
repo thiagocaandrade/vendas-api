bde706fa9d2cd8f092c78d17859e02e068a5654e
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2020-08-07 11:58:00 -0300

    Requisição Get, Post, Delete, Put

--- src/main/java/io/github/thiagocaandrade/rest/controller/ClienteController.java
+++ src/main/java/io/github/thiagocaandrade/rest/controller/ClienteController.java
@@ -27,4 +27,32 @@ public class ClienteController {
 
         return ResponseEntity.notFound().build();
     }
+
+    @PostMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity save(@RequestBody Cliente cliente){
+        Cliente clienteSalvo = clientes.save(cliente);
+        return ResponseEntity.ok(clienteSalvo);
+    }
+
+    @DeleteMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity delete(@PathVariable Integer id){
+        Optional<Cliente> cliente = clientes.findById(id);
+        if (cliente.isPresent()){
+            clientes.delete(cliente.get());
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity update(@PathVariable Integer id, @RequestBody Cliente cliente){
+        return clientes.findById(id).map(clienteExistente -> {
+            cliente.setId(clienteExistente.getId());
+            clientes.save(cliente);
+            return ResponseEntity.noContent().build();
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
