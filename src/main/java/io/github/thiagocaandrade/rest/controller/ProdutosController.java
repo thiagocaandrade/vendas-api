864ead8e94f8b6d7fb8ea5c6ac715fb2ef4fab06
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2020-08-17 11:44:29 -0300

    Validando entidade produto com Bean Validation

--- src/main/java/io/github/thiagocaandrade/rest/controller/ProdutosController.java
+++ src/main/java/io/github/thiagocaandrade/rest/controller/ProdutosController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -31,7 +32,7 @@ public class ProdutosController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Produto save(@RequestBody Produto produto){
+    public Produto save(@RequestBody @Valid Produto produto){
         return produtos.save(produto);
     }
 
@@ -48,7 +49,7 @@ public class ProdutosController {
 
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@PathVariable Integer id, @RequestBody Produto produto){
+    public void update(@PathVariable Integer id, @RequestBody @Valid Produto produto){
         produtos
                 .findById(id)
                 .map( produtoExistente -> {
