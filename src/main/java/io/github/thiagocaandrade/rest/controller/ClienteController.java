d48151e7db0e6efd88ec9893c1a208f0a95fb6e0
thiagocaandrade <thiago.carlos.andrade@gmail.com>
2020-08-18 11:25:54 -0300

    Configurando o Security no Swagger

--- src/main/java/io/github/thiagocaandrade/rest/controller/ClienteController.java
+++ src/main/java/io/github/thiagocaandrade/rest/controller/ClienteController.java
@@ -2,6 +2,7 @@ package io.github.thiagocaandrade.rest.controller;
 
 import io.github.thiagocaandrade.domain.entity.Cliente;
 import io.github.thiagocaandrade.domain.repository.Clientes;
+import io.swagger.annotations.*;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
+@Api("Api Clientes")
 public class ClienteController {
 
     private Clientes clientes;
@@ -22,7 +24,12 @@ public class ClienteController {
     }
 
     @GetMapping("{id}")
-    public Cliente getClienteById( @PathVariable Integer id ){
+    @ApiOperation("Obter detalhes de um cliente")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cliente encontrado"),
+            @ApiResponse(code = 404, message = "Cliente não encontrado para o ID informado")
+    })
+    public Cliente getClienteById( @PathVariable @ApiParam("Id do cliente") Integer id ){
         return clientes
                 .findById(id)
                 .orElseThrow(() ->
@@ -32,6 +39,11 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Salva um novo cliente")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Cliente salvo com sucesso"),
+            @ApiResponse(code = 400, message = "Erro de validação")
+    })
     public Cliente save( @RequestBody @Valid Cliente cliente ){
         return clientes.save(cliente);
     }
